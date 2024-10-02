9436405d781a34997c4e382a212dcc87f5e82ec1
Yvv1214 <94neongreen@gmail.com>
2024-10-02 01:08:24 -0400

    flex box project

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -14,10 +14,12 @@ export default function Home() {
       </div>
       <TechStack />
       <h2 className="text-2xl text-center font-bold">Projects</h2>
-      <div className="border rounded m-4 p-2">
-        <div>images</div>
+      <div className="flex border rounded m-4 p-2">
+        <img src="#" alt="Greengarb" className="border border-green-200 rounded"></img>
+       <div className="column border border-red-200">
         <div>content</div>
         <div>tech used</div>
+      </div>
       </div>
 
     </main>
