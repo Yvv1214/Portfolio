216bfc71c31411a23a1661d0cc0555fd0c87c983
Yvv1214 <94neongreen@gmail.com>
2024-10-06 12:46:16 -0400

    portfolio div basic styled

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -16,10 +16,13 @@ export default function Home() {
       <TechStack />
       <h2 className="text-2xl text-center font-bold">Projects</h2>
       <div className="flex border rounded m-4 p-2">
-        <img className="border border-green-200 rounded" src="#" alt="Greengarb" ></img>
-       <div className="column border border-red-200">
-        <div>content</div>
-        <div>tech used</div>
+        <img className="border border-green-200 rounded w-5/12" src="#" alt="Greengarb" ></img>
+       <div className="column border border-red-200 w-7/12">
+        <div className="p-2 w-50">content shopping markertplace users store user store view from their account filter clothing tags</div>
+        <div className="p-2"> React Python Flask Bootstrap</div>
+        <div className="flex justify-center w-full">
+          <button className="bg-green-400 rounded p-1">Live Preview</button>
+        </div>
       </div>
       </div>
       <Footer/>
