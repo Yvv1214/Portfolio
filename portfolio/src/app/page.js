b7f0de4219bbf61f6632693b95bbe9b706863d7c
Yvv1214 <94neongreen@gmail.com>
2024-10-08 00:50:22 -0400

    basic contribute added

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
       <TechStack />
       <h2 className="text-2xl text-center font-bold">Projects</h2>
-      {/* project div */}
+      {/* project div----------------------------------------------------------------- */}
       <div className="flex border rounded m-4 p-2 shadow-xl mb-12">
         <img className="border rounded w-5/12" src="#" alt="Greengarb" ></img>
         <div className="column w-7/12">
@@ -64,12 +64,12 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {/* Contributions */}
-      <h2>Contributions</h2>
-      <div>
-        <h2>MakeitMVP</h2>
-        <div>video</div>
-        <p>collab with coffechar and memver achievemnt</p>
+      {/* Contributions--------------------------------------------------------------- */}
+      <h2 className="text-2xl text-center font-bold">Contributions</h2>
+      <div className="border rounded m-4 p-2 column-1">
+        <h2 className="text-center font-bold">MakeitMVP</h2>
+        <div className="align-baseline border">video</div>
+        <p className="p-2 border rounded">As a full stack developer at MakeitMVP, I played a key role in the development of CoffeeChat and Member Achievement, two essential features of the Community app. CoffeeChat allows members to connect for networking and collaboration, enhancing user engagement through personalized match-ups. I worked on both the front-end and back-end, ensuring seamless integration and user-friendly design. Additionally, I contributed to the Member Achievement feature, which highlights user milestones and contributions within the community, fostering a sense of accomplishment and encouraging active participation. These features helped boost engagement and strengthen the overall community experience.</p>
       </div>
       <Footer />
 
