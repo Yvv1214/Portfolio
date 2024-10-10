aea7ba96409cbcca405fdaf5b44a15ddb3eecec8
Yvv1214 <94neongreen@gmail.com>
2024-10-09 20:15:06 -0400

    add button

--- portfolio/src/app/Componets/Contact.jsx
+++ portfolio/src/app/Componets/Contact.jsx
@@ -43,6 +43,9 @@ export const Contact = () => {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={''}
             />
+            <div className="flex p-4 justify-center">
+                <button className="w-1/12 border rounded bg-green-400 p-2">Send</button>
+            </div>
         </form>
     )
 }
