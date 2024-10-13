76d132a8ebc1caa15eb82be3f548edc000ca3815
Yvv1214 <94neongreen@gmail.com>
2024-10-13 18:15:24 -0400

    center form

--- portfolio/src/app/Componets/Contact.jsx
+++ portfolio/src/app/Componets/Contact.jsx
@@ -5,8 +5,9 @@ export const Contact = () => {
 
 
     return (
-        <form className="border rounded m-12 bg-cyan-300">
-            <h2 className=" text-bold text-center font-bold text-2xl">Contact</h2>
+        <div className="flex justify-center items-center h-screen">
+        <form className="border rounded m-12 bg-cyan-300 flex flex-col items-center w-1/2">
+            <h2 className=" text-bold text-center font-bold text-2xl p-2">Contact</h2>
             <label
                 htmlFor="Name"
                 className="block text-sm font-medium leading-6 text-gray-900">
@@ -17,19 +18,19 @@ export const Contact = () => {
                 name="Name"
                 type="text"
                 placeholder="Name"
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                className="block flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
             />
             <label
                 htmlFor="Email"
                 className="block text-sm font-medium leading-6 text-gray-900">
-                Name
+                Email
             </label>
             <input
                 id="Email"
                 name="Email"
                 type="email"
                 placeholder="Email"
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                className="block flex-1 border-0 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
             />
             <label
                 htmlFor="Message"
@@ -43,9 +44,10 @@ export const Contact = () => {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-600 sm:text-sm sm:leading-6 w-11/12"
                 defaultValue={''}
             />
-            <div className="flex p-4 justify-center">
-                <button className="w-1/12 border rounded bg-green-400 p-2">Send</button>
+            <div className="flex p-4 justify-center w-6/12">
+                <button className="border rounded bg-green-400 p-2">Send</button>
             </div>
         </form>
+        </div>
     )
 }
