a192a7d0e028b4f5d6ffdff7dcfb6e36777534e2
Yvv1214 <94neongreen@gmail.com>
2024-11-05 10:34:38 -0500

    organize files

--- Backend/django/project_name/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UploadedImage(models.Model):
-    title = models.CharField(max_length=100)  # Optional: a title for the image
-    image = models.ImageField(upload_to='images/')  # Set directory under MEDIA_ROOT
-
-    def __str__(self):
-        return self.title
