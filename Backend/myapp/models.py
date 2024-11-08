acc0ebbb1f89f9f0efbc829f6f05584abadb45dd
Yvv1214 <94neongreen@gmail.com>
2024-11-08 12:37:07 -0500

    video file

--- Backend/myapp/models.py
+++ Backend/myapp/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class UploadedImage(models.Model):
     title = models.CharField(max_length=100)  # title for the image
     image = models.ImageField(upload_to='images/')  # Set directory under MEDIA_ROOT
+    project_type = models.CharField(max_length=100) # which project these images belong to
 
-    def __str__(self):
-        return self.title
+instance = UploadedImage.objects.first()  # Get an instance
+image_url = instance.image.url  # This will give you the URL to the image
+
+
+def __str__(self):
+    return self.title
